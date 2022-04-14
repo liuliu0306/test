c6fb155e443e35e7d531a518b3b48603f3907b19
liuliu0306 <996125554@qq.com>
2022-04-14 22:01:45 +0800

    Update Mutable.py

--- Mutable.py
+++ Mutable.py
@@ -44,3 +44,52 @@ class UnrolledLinkedList:
                 state = f(state, cur.elements[i])
             cur = cur.next
         return state
+   
+    def remove(self, idx):
+        if idx < 0 or idx >= self.total_size:
+            return
+
+        # 找到删除元素的节点和位置
+        cur = self.head.next
+        while idx >= cur.numElements - 1:
+            if idx == cur.numElements - 1:
+                break
+            idx -= cur.numElements
+            cur = cur.next
+
+        # 删除元素
+        for i in range(idx, cur.numElements - 1, 1):
+            cur.elements[i] = cur.elements[i + 1]
+        cur.elements[cur.numElements - 1] = None
+        cur.numElements -= 1
+
+        if cur.next.cap != -1 and cur.cap >= cur.numElements + cur.next.numElements:
+            # 合并删除元素节点的下一节点至当前节点
+            next = cur.next
+            for i in range(0, next.numElements):
+                cur.elements[cur.numElements + i] = next.elements[i]
+            cur.numElements += next.numElements
+            cur.next = next.next
+
+        self.total_size -= 1
+
+    def get(self, idx):
+        if idx < 0 or idx >= self.total_size:
+            return None
+
+        cur = self.head.next
+        while idx >= cur.numElements:
+            idx -= cur.numElements
+            cur = cur.next
+        return cur.elements[idx]
+
+    def is_member(self, member):
+        cur = self.head.next
+        count = 0
+        while cur is not None:
+            for i in range(0, cur.numElements):
+                count = count + 1
+                if member == cur.elements[i]:
+                    index = count - 1
+                    return index
+            return -1
