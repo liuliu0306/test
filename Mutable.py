32835f8996b1fe327fd8201c8b7f09eaa8502edb
liuliu0306 <996125554@qq.com>
2022-04-14 21:14:46 +0800

    Update Mutable.py

--- Mutable.py
+++ Mutable.py
@@ -53,48 +53,6 @@ class UnrolledLinkedList:
             cur = cur.next
         return res
 
-
-    def set(self, idx, obj):
-        if idx < 0 or idx > self.total_size:
-            return
-
-        # 找到插入节点和位置
-        cur = self.head.next
-        while idx >= cur.numElements:
-            if idx == cur.numElements:
-                break
-            idx -= cur.numElements
-            cur = cur.next
-
-        if cur.numElements == cur.cap:
-            # 插入节点已满，创建新节点
-            node = Node()
-            next = cur.next
-            cur.next = node
-            node.next = next
-
-            # 将插入节点一般元素移至新节点
-            move_idx = cur.numElements // 2
-            for i in range(move_idx, cur.numElements):
-                node.elements[i - move_idx] = cur.elements[i]
-                cur.elements[i] = None
-                cur.numElements -= 1
-                node.numElements += 1
-
-            # 更新插入位置
-            if idx >= move_idx:
-                idx -= move_idx
-                cur = node
-
-        # 插入元素
-        for i in range(cur.numElements - 1, idx - 1, -1):
-            cur.elements[i + 1] = cur.elements[i]
-        cur.elements[idx] = obj
-
-        cur.numElements += 1
-        self.total_size += 1
-
-
     def remove(self, idx):
         if idx < 0 or idx >= self.total_size:
             return
@@ -144,25 +102,4 @@ class UnrolledLinkedList:
                     return index
             return -1
 
-    def filter(self, f):
-        cur = self.head.next
-        for i in range(0, cur.numElements):
-            cur.elements[i] = f(cur.elements[i])
-        return self.to_list()
-
-    def map(self, f):
-        cur = self.head.next
-        while cur is not None:
-            for i in range(0, cur.numElements):
-                cur.elements[i] = f(cur.elements[i])
-            cur = cur.next
-
-    def reduce(self, f, initial_state):
-        state = initial_state
-        cur = self.head.next
-        while cur is not None:
-            for i in range(0, cur.numElements):
-                state = f(state, cur.elements[i])
-            cur = cur.next
-        return state
 
